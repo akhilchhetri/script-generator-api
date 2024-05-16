db2a448d18a7f1a5d3b5e7e333ed1a9761f4f729
Akhil Chhetri <akhyl.78@gmail.com>
2024-05-17 00:20:50 +0545

    update: new api implementation

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import httpx
 import requests
 from fastapi import FastAPI, File, Request, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 origins = ["*"]
 base_api = (
-    "http://script-generation-demo-1979457470.eu-north-1.elb.amazonaws.com/api/v1"
+    "http://script-generation-prod-1695652430.eu-north-1.elb.amazonaws.com/api/v1"
 )
 app = FastAPI()
 
@@ -29,14 +30,23 @@ async def root():
 
 @app.post("/upload")
 async def upload_doc(files: UploadFile = File(...)):
+    url = "http://script-generation-prod-1695652430.eu-north-1.elb.amazonaws.com/api/v1/syllabus/extract/"
+    params = {
+        "document_type": "research_paper",
+        "use_llm": "false",
+        "language": "British English",
+    }
+    files = {"pdf_file": (files.filename, files.file, "application/pdf")}
+
     try:
-        contents = await files.read()
-        api_data = await upload_file(contents, files)
-        if api_data:
-            return api_data
+        response = requests.post(url, params=params, files=files)
+        if response.status_code == 200:
+            data = response.json()
+            return data
         else:
             return {"message": "API call failed"}
-    except Exception as e:
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
         return {"message": "API call failed", "error": e}
 
 
@@ -44,10 +54,15 @@ async def upload_doc(files: UploadFile = File(...)):
 async def script_generate(request: Request):
     try:
         data = await request.json()
+        params = {
+            "language": "British English",
+        }
         response = requests.post(
             base_api + "/script/generate/",
+            params=params,
             json=data,
         )
+        print("this is response", response.json())
         if response.status_code == 200:
             res = response.json()
             return res
@@ -61,8 +76,10 @@ async def script_generate(request: Request):
 async def qna_generate(request: Request):
     try:
         data = await request.json()
-        response = requests.post(base_api + "/qna/generate/", json=data)
-        print("this is response", response.json())
+        params = {
+            "language": "British English",
+        }
+        response = requests.post(base_api + "/qna/generate/", params=params, json=data)
         if response.status_code == 200:
             res = response.json()
             return res
@@ -76,7 +93,6 @@ async def qna_generate(request: Request):
 async def chat(request: Request):
     try:
         data = await request.json()
-        print("this is data", data)
         response = requests.post(base_api + "/chat/", json=data)
         print("this is response", response.json())
         if response.status_code == 200:
@@ -89,27 +105,60 @@ async def chat(request: Request):
         return {"success": False, "error": e}
 
 
+# Syllabus update method
 @app.patch("/syllabus/update")
 async def syllabus_update(request: Request):
     try:
         data = await request.json()
-        response = requests.patch(base_api + "/syllabus/update/", json=data)
-        print("this is response", response.json())
+        print(data["id"])
+        params = {"heading_id": data["id"]}
+        response = requests.patch(
+            base_api + f"/syllabus/update/", params=params, json=data
+        )
         if response.status_code == 200:
             res = response.json()
             return res
         else:
-            return {"message": "Api call failed", "code": "500", "type": "error"}
+            return {
+                "error": True,
+                "message": "Api call failed",
+                "code": "500",
+                "type": "error",
+            }
     except Exception as e:
-        return {"success": False, "error": e}
+        print("this is exception", e)
+        return {"success": False, "error": True, "message": e}
+
+
+# Syllabus delete method
+@app.post("/syllabus/delete")
+async def syllabus_delete(request: Request):
+    try:
+        data = await request.json()
+        params = {"heading_id": data["id"]}
+        response = requests.delete(base_api + f"/syllabus/delete/", params=params)
+        if response.status_code == 200:
+            res = response.json()
+            return res
+        else:
+            return {
+                "error": True,
+                "message": "Api call failed",
+                "code": "500",
+                "type": "error",
+            }
+    except Exception as e:
+        print("this is exception", e)
+        return {"success": False, "error": True, "message": e}
 
 
 @app.patch("/script/update")
-async def script_update(request: Request):
+async def script_update(request: Request, script_id):
     try:
         data = await request.json()
-        response = requests.patch(base_api + "/script/update", json=data)
-        print("this is response", response.json())
+        response = requests.patch(
+            base_api + f"/script/update?script_id={script_id}", json=data
+        )
         if response.status_code == 200:
             res = response.json()
             return res
@@ -120,10 +169,27 @@ async def script_update(request: Request):
 
 
 @app.patch("/qna/update")
-async def script_update(request: Request):
+async def script_update(request: Request, question_id: str):
     try:
         data = await request.json()
-        response = requests.patch(base_api + "/qna/update", json=data)
+        response = requests.patch(
+            base_api + f"/qna/update?question_id={question_id}", json=data
+        )
+        print("this is response", response.json())
+
+        if response.status_code == 200:
+            res = response.json()
+            return res
+        else:
+            return {"message": "Api call failed", "code": "500", "type": "error"}
+    except Exception as e:
+        return {"success": False, "error": e}
+
+
+@app.post("/qna/delete")
+async def script_update(question_id: str):
+    try:
+        response = requests.delete(base_api + f"/qna/delete?question_id={question_id}")
         print("this is response", response.json())
 
         if response.status_code == 200:
@@ -137,18 +203,32 @@ async def script_update(request: Request):
 
 async def upload_file(contents, file):
     async with httpx.AsyncClient() as client:
-        files = {
-            "pdf_file": (file.filename, BytesIO(contents), "application/octet-stream")
-        }
+        try:
+            files = {
+                "pdf_file": (
+                    file.filename,
+                    BytesIO(contents),
+                    "application/octet-stream",
+                )
+            }
+            params = {
+                "document_type": "research_paper",
+                "use_llm": "false",
+                "language": "British English",
+            }
 
-        response = await client.post(
-            "http://script-generation-demo-1979457470.eu-north-1.elb.amazonaws.com/api/v1/syllabus/extract/",
-            files=files,
-        )
-        if response.status_code == 200:
-            data = response.json()
-            return data
-        else:
+            response = await client.post(
+                base_api + "/syllabus/extract/",
+                params=params,
+                files=files,
+            )
+            if response.status_code == 200:
+                data = response.json()
+                return data
+            else:
+                return None
+        except Exception as e:
+            print("this is error", e)
             return None
 
 
